c22f0bd77dd92c408fad07c36fe78f1829f6cb01
Ruben Jenster <rjenster@gmail.com>
2014-02-04 20:56:48 +0100

    Added doxygen configuration. Document message.h.

--- src/message.h
+++ src/message.h
@@ -4,26 +4,56 @@
 
 typedef enum
 {
-  SUBSCRIBE, UNSUBSCRIBE, PUBLISH
+  SUBSCRIBE, /*!< subscribe to a topic */
+  UNSUBSCRIBE, /*!< unsubscribe from a topic */
+  PUBLISH /*!< publish a message to a topic */
 } Method;
 
+/*!
+ * The message
+ */
 typedef struct message_t
 {
   Method method;
-  const char *topic;
+  const char *topic; /*!< the topic */
   const char *data;
   char *serialized;
   int size;
 } Message;
 
+/*!
+ * Creates a new message.
+ *
+ * @param[out] msg The pointer to the message that is created.
+ * @param[in] method The method of the message.
+ * @param[in] topic The path of the queue to which the message is send (e.g /foo/bar).
+ * @param[in] data The payload for the message.
+ */
 void
 message_new(Message *msg, Method method, const char *topic, const char *data);
 
+/*!
+ * Parses an incomming message.
+ *
+ * @param[out] msg The pointer to the message that is created.
+ * @param[in] data The serialized message.
+ */
 int
 message_parse(Message *msg, const char *data);
 
+/*!
+ * Serializes the given message.
+ * The serialized message is stored in the message itself.
+ *
+ * @param[in,out] msg The message which is serialized.
+ */
 void
 message_serialize(Message *msg);
 
+/*!
+ * Frees the memory allocated for the message, including the serialized form.
+ *
+ * @param[in] msg The message to free.
+ */
 void
 message_free(Message *msg);
