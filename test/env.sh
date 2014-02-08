af229383b3ce7f467a47b0c2aeb7eb05c2dde510
Ruben Jenster <rjenster@gmail.com>
2014-02-08 10:17:01 +0100

    Switched to latest unity-test-seed Makefile.

--- test/env.sh
+++
@@ -1,7 +0,0 @@
-BREW_LIBS="libwebsockets libev mosquitto jansson"
-CFLAGS="$CFLAGS `brewlibs $BREW_LIBS`"
-
-SRC="variable_scope.c ../src/subscriber.c ../src/message.c ../src/llog.c"
-
-export CFLAGS
-export SRC
